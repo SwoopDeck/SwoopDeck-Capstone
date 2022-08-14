a0b66cc7b9958340cc3c3579b626fabc781b6448
Chris Tomshack <ctomshack15@gmail.com>
2022-08-14 19:22:34 -0400

    FEAT: Completed Sign Up component with functioning route to db

--- client/components/CreateUser.js
+++ client/components/CreateUser.js
@@ -1,16 +1,112 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
+import { createUser } from '../store/allUsers';
 
-export class CreateUser extends Component {
+export class CreateUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.createUser(this.state);
+  }
   render() {
     return (
-      <div>CreateUser</div>
-    )
+      <div className='login-main'>
+      {/* <Navbar /> */}
+      <div className='logo-container'>
+
+      <img src="/assets/SwoopLog.png" alt="logo" className='centered-logo'/>
+      </div>
+      <div className="create-form form div-container">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-container signup">
+            <div className="signup-card">
+              <p className="title">Create A New Account</p>
+              <div className="input-container">
+                <label className="labelName">
+                  <input
+                    type="text"
+                    name="firstName"
+                    placeholder="First name"
+                    onChange={this.handleChange}
+                    value={this.state.firstName}
+                  />
+                </label>
+              </div>
+
+              <div className="input-container">
+                <label className="labelName">
+                  <input
+                    className="input"
+                    type="text"
+                    name="lastName"
+                    placeholder="Last name"
+                    onChange={this.handleChange}
+                    value={this.state.lastName}
+                  />
+                </label>
+              </div>
+
+              <div className="input-container">
+                <label className="labelName">
+                  <input
+                    className="input"
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    onChange={this.handleChange}
+                    value={this.state.email}
+                  />
+                </label>
+              </div>
+
+              <div className="input-container">
+                <label className="labelName">
+                  <input
+                    className="input"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                  />
+                </label>
+              </div>
+              <div>
+                <button className="button submit-btn buttonShadow" type="submit">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => ({})
+const mapDispatchToProps = (dispatch, { history }) => {
+  return {
+    createUser: (props) => dispatch(createUser(props, history)),
+  };
+};
 
-const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CreateUser)
+export default connect(null, mapDispatchToProps)(CreateUser);
