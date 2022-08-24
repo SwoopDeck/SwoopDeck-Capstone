9944e627f69d5ffe64faba8d5c12dde9e2688ac1
Chris Tomshack <ctomshack15@gmail.com>
2022-08-24 13:07:13 -0400

    FE&BE: AllUsers.js component

--- client/App.js
+++ client/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
           <Sidebar />
           <ToastContainer />
           <Routes />
-          <Copyright /> 
+          {/* <Copyright />  */}
           </div>
         ) : (
           <div> 
--- client/store/allUsers.js
+++ client/store/allUsers.js
@@ -6,6 +6,8 @@ import { me } from './auth'
 
 const CREATE_USER = 'CREATE_USER';
 const SET_USERS = 'SET_USERS';
+const UPDATE_USER = 'UPDATE_USER';
+const DELETE_USER = 'DELETE_USER';
 
 
 
@@ -27,6 +29,21 @@ export const _createUser = (user) => {
   };
 };
 
+// UPDATE A USER
+export const _updateUser = (user) => {
+  return {
+    type: UPDATE_USER,
+    user,
+  };
+};
+
+// DELETE A USER
+export const _deleteUser = (user) => {
+  return {
+    type: DELETE_USER,
+    user,
+  };
+};
 
 
 
@@ -43,9 +60,35 @@ export const createUser = (user, history) => {
   };
 };
 
+// THUNK: UPDATE A USER
+export const Thunk_updateUser = (id, history) => {
+  return async (dispatch) => {
+    try {
+      const { data: user } = await axios.put(`/api/users/${id}`);
+      dispatch(_updateUser(user));
+      history.push('/users');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
+// THUNK: DELETE A USER
+export const Thunk_deleteUser = (id, history) => {
+  return async (dispatch) => {
+    try {
+      const { data: user } = await axios.put(`/api/users/${id}`);
+      dispatch(_deleteUser(user));
+      history.push('/users');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
 
 // THUNK: FETCH ALL USERS
-export const fetchUsers = () => {
+export const Thunk_fetchUsers = () => {
   return async (dispatch) => {
     try {
       const token = window.localStorage.getItem('token');
@@ -73,9 +116,14 @@ const initialState = [];
 export default function usersReducer(state = initialState, action) {
   switch (action.type) {
     case CREATE_USER:
-      return action.user;
+      return [...state, action.user];
     case SET_USERS:
+      console.log(action.users)
       return action.users;
+    case UPDATE_USER:
+      return action.user;
+    case DELETE_USER:
+      return state.filter((users) => users.id !== action.users.id);
     default:
       return state;
   }
