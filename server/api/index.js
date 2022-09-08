ba356826c6283d9715432f8794636fb6f78582a3
Apple <apple@apples-mbp.mynetworksettings.com>
2022-09-08 19:17:02 -0400

    fourth try

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.use('/users', require('./users'))
-router.use('/jumprecords', require('./jumprecords'));
+router.use('/jumpRecords', require('./jumpRecords'));
 router.use('/dropzones', require('./dropzones'));
 router.use('/loads', require('./loads'));
 router.use('/stripe', require('./stripe'))
